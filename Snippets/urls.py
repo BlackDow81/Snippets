c195b02781b86da333d682bee5767baaa79f4958
boo <eyurchenko@specialist.ru>
2022-09-26 21:35:00 +0300

    add Snippet Form

--- Snippets/urls.py
+++ Snippets/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     path('snippets/add', views.add_snippet_page, name='snippets-add'),
     path('snippets/list', views.snippets_page, name='snippets-list'),
     path('snippet/<int:snippet_id>', views.snippet_detail, name='snippet-detail'),
-]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+ ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
